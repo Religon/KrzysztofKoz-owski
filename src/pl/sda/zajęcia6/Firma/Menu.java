23eba42b007b2e897b7be091ff53e16c7013748d
uzytkownik <newphanton2@gmail.com>
2020-02-15 15:09:54 +0100

    Firma*2

--- src/pl/sda/zajęcia6/Firma/Menu.java
+++ src/pl/sda/zajęcia6/Firma/Menu.java
@@ -3,12 +3,44 @@ package pl.sda.zajęcia6.Firma;
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
-        Firma f = new Firma("Krzysztof", "Kozłowski", 'M', 100, 2500, 26,0, false);
-        Scanner cin = new Scanner(System.in);
+    private final static int domyślny_rozmiar_listy = 100;
+    private Firma[] lista;
+    private int rozmiar;
 
-        f.display_employe();
-        f.display_less();
-        f.display_special();
+
+    ////konstruktor
+    public Menu(){this(domyślny_rozmiar_listy);}
+
+    public Menu(int rozmiar){
+        lista = new Firma[rozmiar];
+    }
+    //////////////////////
+
+    ///////////
+    ////metody
+    ///////////
+
+
+    ////////////////
+    //settery i gettery
+    ////////////
+    public static int getDomyślny_rozmiar_listy() {
+        return domyślny_rozmiar_listy;
+    }
+
+    public Firma[] getLista() {
+        return lista;
+    }
+
+    public void setLista(Firma[] lista) {
+        this.lista = lista;
+    }
+
+    public int getRozmiar() {
+        return rozmiar;
+    }
+
+    public void setRozmiar(int rozmiar) {
+        this.rozmiar = rozmiar;
     }
 }
